4f764c3709df32a90ecf432d045996fbe6b4bd13
Maxim Khlamov <mkhlamov@gmail.com>
2018-09-11 11:26:23 +0300

    Добавлено выделение клетки

---
+++ ChessProject/Assets/Scripts/TileSelector.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSelector : MonoBehaviour {
+
+    public GameObject tileHighlightPrefab;
+
+    private GameObject tileHighlight;
+
+
+    // Use this for initialization
+    void Start () {
+        Vector2Int gridPoint = Geometry.GridPoint(0, 0);
+        Vector3 point = Geometry.PointFromGrid(gridPoint);
+        tileHighlight = Instantiate(tileHighlightPrefab, point, Quaternion.identity, gameObject.transform);
+        tileHighlight.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 point = hit.point;
+            Vector2Int gridPoint = Geometry.GridFromPoint(point);
+
+            tileHighlight.SetActive(true);
+            tileHighlight.transform.position =
+                Geometry.PointFromGrid(gridPoint);
+        }
+        else
+        {
+            tileHighlight.SetActive(false);
+        }
+    }
+
+    public void EnterState()
+    {
+        enabled = true;
+    }
+}
